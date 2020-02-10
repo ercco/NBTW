436fba41d9ebf16d4dee314a266838321c3bd047
Tarmo Nurmi <tarmo.nurmi@aalto.fi>
2020-02-10 16:57:16 +0200

    pop unnecessary items, handle case k == 1

--- nbtwc.py
+++ nbtwc.py
@@ -5,10 +5,13 @@ def get_nbtw_centrality(A,k):
     # k: int (natural)
     assert len(A.shape) == 2 and A.shape[0] == A.shape[1]
     assert np.allclose(A.T,A)
+    if k == 1:
+        return A
     D = np.diag(np.matmul(A,np.ones(A.shape[0])))
     P = []
     P.append(A)
     P.append(np.matmul(A,A)-D)
     for ii in range(2,k):
-        P.append(np.matmul(A,P[ii-1]) - np.matmul((D-np.identity(A.shape[0])),P[ii-2]))
-    return P[-1]
+        P.append(np.matmul(A,P[1]) - np.matmul((D-np.identity(A.shape[0])),P[0]))
+        P.pop(0)
+    return P[1]
