cebd4c89064fb6a38d5ba7eb6abf0925b8ce4a51
Tarmo Nurmi <tarmo.nurmi@aalto.fi>
2020-02-10 18:06:58 +0200

    add case k == 0 (1 by def)

--- nbtwc.py
+++ nbtwc.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-def get_nbtw_centrality(A,k):
+def get_nbtws(A,k):
     # A: numpy matrix
-    # k: int (>= 1)
+    # k: int (>= 0)
     assert len(A.shape) == 2 and A.shape[0] == A.shape[1]
     assert np.allclose(A.T,A)
     assert k >= 1
+    if k == 0:
+        return np.identity(A.shape[0]) # one self-walk of length 0 by definition
     if k == 1:
         return A
     D = np.diag(np.matmul(A,np.ones(A.shape[0])))
